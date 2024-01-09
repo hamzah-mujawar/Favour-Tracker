e82b88a15a12eb1f39487a3e44449b50c85f0fe5
Rahat <rahat@pop-os.localdomain>
2024-01-09 10:46:22 +0000

    Add logic to initialise firebase app

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,20 @@
-import express, {Express, Request, Response} from "express";
-const server: Express = express();
+import express  from "express";
+import admin from "firebase-admin";
+const server = express();
 const port: number = 3000;
 
-server.get("/", (req: Request, res: Response) => {
+let fbase_admin_key_path: string = process.env.FIREBASE_ADMIN_KEY_PATH || "";
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(require(fbase_admin_key_path))
+    });
+} catch(e) {
+    console.error(`Error occured while initialising firebase app: ${e}`)
+    process.exit(1);
+}
+
+
+server.get("/", (_, res) => {
     res.send("Hello from FavourTracker server!");
 })
 
