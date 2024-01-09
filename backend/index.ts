221f291767625625638fce6c360ef543dae856a6
Rahat <rahat@pop-os.localdomain>
2024-01-09 10:51:18 +0000

    Added some comments

--- backend/index.ts
+++ backend/index.ts
@@ -3,12 +3,14 @@ import admin from "firebase-admin";
 const server = express();
 const port: number = 3000;
 
+// Use process environment variables to obtain the path to firebase admin key JSON file.
 let fbase_admin_key_path: string = process.env.FIREBASE_ADMIN_KEY_PATH || "";
 try {
     admin.initializeApp({
         credential: admin.credential.cert(require(fbase_admin_key_path))
     });
 } catch(e) {
+    // If an error happens nicely print it and exit.
     console.error(`Error occured while initialising firebase app: ${e}`)
     process.exit(1);
 }
